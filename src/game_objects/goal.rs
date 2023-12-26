01bdef3dc0baf93add26a88056d27d2d0d3832e1
inosms <4201657+inosms@users.noreply.github.com>
2023-12-26 20:37:41 +0900

    [modify] improve end

--- src/game_objects/goal.rs
+++ src/game_objects/goal.rs
@@ -34,69 +34,74 @@ pub fn check_goal_reached_system(
 }
 
 fn string_to_level(input: &str) -> String {
-    let mut output = player_start_pos();
+    let mut output = String::new();
     for line in input.lines() {
         output.push_str(&line_to_level(line));
         output.push_str("\n");
     }
+    // add the player "+P" at the first cell (that is before the first whitespace following a character)
+    let mut output = output.trim().to_string();
+    let first_whitespace_index = output.find(' ').unwrap();
+    output.insert_str(first_whitespace_index, "+P");
     output
 }
 
 fn line_to_level(input: &str) -> String {
-    let mut output_lines = vec![String::new(); 6];
+    let mut output_lines = vec![String::new(); 7];
 
     for block in input.chars() {
         let block = letter_to_level_block(block);
+        let block_length = block.lines().next().unwrap().trim().split_ascii_whitespace().count() + 2;
+        let padding = " N+N ".repeat(block_length);
+        // add padding to the beginning of the line
+        output_lines[0].push_str(&padding);
+
         let lines = block.lines();
         for (i, line) in lines.enumerate() {
             // remove newline character and add
             let trimmed_line = line.trim();
-            output_lines[i].push_str(trimmed_line);
-            output_lines[i].push_str(" N+W ");
+            output_lines[i+1].push_str(" N+N ");
+            output_lines[i+1].push_str(trimmed_line);
+            output_lines[i+1].push_str(" N+N ");
 
         }
         // add padding to the end of the line 
-        let block_length = block.lines().next().unwrap().trim().split_ascii_whitespace().count() + 1;
-        let padding = " N+W ".repeat(block_length);
-        output_lines[5].push_str(&padding);
+        output_lines[6].push_str(&padding);
     }
 
     // concatenate all lines to a single string
     output_lines.join("\n")
 }
 
-fn player_start_pos() -> String {
-    "N+W N+W N+W\nN+W N+W N+W\nN+W N+W N+W+P\nN+W N+W N+W\nN+W N+W N+W\n".to_string()
-}
 
 fn letter_to_level_block(c: char) -> String {
     match c {
-        'A' => "N+W N+BRF N+BRF N+W\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF".to_string(),
-        'B' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        'C' => "N+W N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W\nN+W N+BRF N+BRF N+BRF".to_string(),
-        'D' => "N+BRF N+BRF N+BRF N+W\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+W".to_string(),
-        'E' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        'F' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W".to_string(),
-        'G' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+W N+BRF N+BRF\nN+BRF N+W N+BRF N+BRF\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        'H' => "N+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF".to_string(),
-        'I' => "N+W N+BRF N+W N+W\nN+W N+BRF N+W N+W\nN+W N+BRF N+W N+W\nN+W N+BRF N+W N+W\nN+W N+BRF N+W N+W".to_string(),
-        'J' => "N+W N+W N+W N+BRF\nN+W N+W N+W N+BRF\nN+W N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+W N+BRF N+BRF N+W".to_string(),
-        'K' => "N+BRF N+W N+W N+BRF\nN+BRF N+W N+BRF N+W\nN+BRF N+BRF N+W N+W\nN+BRF N+W N+BRF N+W\nN+BRF N+W N+W N+BRF".to_string(),
-        'L' => "N+BRF N+W N+W N+W\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        'M' => "N+BRF N+W N+W N+W N+BRF\nN+BRF N+BRF N+W N+BRF N+BRF\nN+BRF N+W N+BRF N+W N+BRF\nN+BRF N+W N+W N+W N+BRF\nN+BRF N+W N+W N+W N+BRF".to_string(),
-        'N' => "N+BRF N+W N+W N+BRF\nN+BRF N+BRF N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF".to_string(),
-        'O' => "N+W N+BRF N+BRF N+W\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+W N+BRF N+BRF N+W".to_string(),
-        'P' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+W N+W N+W".to_string(),
-        'Q' => "N+W N+BRF N+BRF N+W\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+W N+BRF N+BRF N+BRF".to_string(),
-        'R' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+BRF N+W\nN+BRF N+W N+W N+BRF".to_string(),
-        'S' => "N+BRF N+BRF N+BRF N+BRF\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF\nN+W N+W N+W N+BRF\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        'T' => "N+BRF N+BRF N+BRF\nN+W N+BRF N+W\nN+W N+BRF N+W\nN+W N+BRF N+W\nN+W N+BRF N+W".to_string(),
-        'U' => "N+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+W N+BRF N+BRF N+W".to_string(),
-        'V' => "N+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+BRF N+W N+W N+BRF\nN+W N+BRF N+BRF N+W".to_string(),
-        'W' => "N+BRF N+W N+W N+W N+BRF\nN+BRF N+W N+W N+W N+BRF\nN+BRF N+W N+W N+W N+BRF\nN+BRF N+W N+BRF N+W N+BRF\nN+W N+BRF N+W N+BRF N+W".to_string(),
-        'X' => "N+BRF N+W N+W N+BRF\nN+W N+BRF N+BRF N+W\nN+W N+BRF N+BRF N+W\nN+W N+BRF N+BRF N+W\nN+BRF N+W N+W N+BRF".to_string(),
-        'Y' => "N+BRF N+W N+BRF\nN+BRF N+W N+BRF\nN+BRF N+BRF N+BRF\nN+W N+BRF N+W\nN+W N+BRF N+W".to_string(),
-        'Z' => "N+BRF N+BRF N+BRF N+BRF\nN+W N+W N+BRF N+W\nN+W N+BRF N+W N+W\nN+BRF N+W N+W N+W\nN+BRF N+BRF N+BRF N+BRF".to_string(),
-        _ => "N+W N+W\nN+W N+W\nN+W N+W\nN+W N+W\nN+W N+W".to_string(),
+        'A' => "N+N N+W N+W N+N\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W".to_string(),
+        'B' => "N+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W".to_string(),
+        'C' => "N+N N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+N N+N N+N\nN+W N+N N+N N+N\nN+N N+W N+W N+W".to_string(),
+        'D' => "N+W N+W N+W N+N\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+N".to_string(),
+        'E' => "N+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+W N+W N+W".to_string(),
+        'F' => "N+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+N N+N N+N".to_string(),
+        'G' => "N+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+N N+W N+W\nN+W N+N N+W N+W\nN+W N+W N+W N+W".to_string(),
+        'H' => "N+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W".to_string(),
+        'I' => "N+N N+W N+N N+N\nN+N N+W N+N N+N\nN+N N+W N+N N+N\nN+N N+W N+N N+N\nN+N N+W N+N N+N".to_string(),
+        'J' => "N+N N+N N+N N+W\nN+N N+N N+N N+W\nN+N N+N N+N N+W\nN+W N+N N+N N+W\nN+N N+W N+W N+N".to_string(),
+        'K' => "N+W N+N N+N N+W\nN+W N+N N+W N+N\nN+W N+W N+N N+N\nN+W N+N N+W N+N\nN+W N+N N+N N+W".to_string(),
+        'L' => "N+W N+N N+N N+N\nN+W N+N N+N N+N\nN+W N+N N+N N+N\nN+W N+N N+N N+N\nN+W N+W N+W N+W".to_string(),
+        'M' => "N+W N+N N+N N+N N+W\nN+W N+W N+N N+W N+W\nN+W N+N N+W N+N N+W\nN+W N+N N+N N+N N+W\nN+W N+N N+N N+N N+W".to_string(),
+        'N' => "N+W N+N N+N N+W\nN+W N+W N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W".to_string(),
+        'O' => "N+N N+W N+W N+N\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+N N+W N+W N+N".to_string(),
+        'P' => "N+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+N N+N N+N".to_string(),
+        'Q' => "N+N N+W N+W N+N\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+N N+W N+W N+W".to_string(),
+        'R' => "N+W N+W N+W N+W\nN+W N+N N+N N+W\nN+W N+W N+W N+W\nN+W N+N N+W N+N\nN+W N+N N+N N+W".to_string(),
+        'S' => "N+W N+W N+W N+W\nN+W N+N N+N N+N\nN+W N+W N+W N+W\nN+N N+N N+N N+W\nN+W N+W N+W N+W".to_string(),
+        'T' => "N+W N+W N+W\nN+N N+W N+N\nN+N N+W N+N\nN+N N+W N+N\nN+N N+W N+N".to_string(),
+        'U' => "N+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+N N+W N+W N+N".to_string(),
+        'V' => "N+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+W N+N N+N N+W\nN+N N+W N+W N+N".to_string(),
+        'W' => "N+W N+N N+N N+N N+W\nN+W N+N N+N N+N N+W\nN+W N+N N+N N+N N+W\nN+W N+N N+W N+N N+W\nN+N N+W N+N N+W N+N".to_string(),
+        'X' => "N+W N+N N+N N+W\nN+N N+W N+W N+N\nN+N N+W N+W N+N\nN+N N+W N+W N+N\nN+W N+N N+N N+W".to_string(),
+        'Y' => "N+W N+N N+W\nN+W N+N N+W\nN+W N+W N+W\nN+N N+W N+N\nN+N N+W N+N".to_string(),
+        'Z' => "N+W N+W N+W N+W\nN+N N+N N+W N+N\nN+N N+W N+N N+N\nN+W N+N N+N N+N\nN+W N+W N+W N+W".to_string(),
+        _ => "N+N N+N\nN+N N+N\nN+N N+N\nN+N N+N\nN+N N+N".to_string(),
     }
 }
